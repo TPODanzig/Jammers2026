09bc55660d7dffde0732041cfa68eb1add8df1b2
nathan lammers <nathanlammers01@gmail.com>
2026-01-28 14:31:18 +0100

    player controlls timer and checkpoint

---
+++ Assets/Scripts/Nathan/timer.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    private float countDown = 60;
+    private float roundCountDown ;
+
+    private void FixedUpdate()
+    {
+        countDown -= Time.deltaTime;
+        roundCountDown = (int)countDown;
+        timerText.text = roundCountDown.ToString();
+    }
+}
+
+
