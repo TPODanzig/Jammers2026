f625d917e260419e46f7704b15d3930a7bc912c9
ellamvdh <fungirl2050@gmail.com>
2026-01-29 17:55:10 +0100

    snake is better

--- Assets/CanvasSnake.cs
+++ Assets/CanvasSnake.cs
@@ -5,10 +5,24 @@ public class CanvasSnake : MonoBehaviour
     [SerializeField] private float _speed = 5f;
     [SerializeField] private Rigidbody2D rb;
     private Vector2 _moveInput;
-    public int FoodAmount;
+    public int FoodAmount = 0;
 
     private MaskPickup parentScript;
+    [SerializeField] GameObject _snakeGame;
 
+
+
+
+   
+
+    enum Flipped
+    {
+        flipedRight,
+        flippedLeft,
+        flippedUp,
+        flippedDown
+    }
+    Flipped flippedVAR = Flipped.flipedRight;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -24,11 +38,37 @@ public class CanvasSnake : MonoBehaviour
         _moveInput.x = inputX * _speed * Time.deltaTime;
         transform.Translate(_moveInput);
 
+
+        if (flippedVAR == Flipped.flipedRight)
+        {
+            this.gameObject.transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
+            this.gameObject.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        else if (flippedVAR == Flipped.flippedLeft) 
+        {
+            this.gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            this.gameObject.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        if (inputX < 0)
+        {
+            flippedVAR = Flipped.flippedLeft;
+        }
+        else if (inputX > 0)
+        {
+            flippedVAR = Flipped.flipedRight;
+        }else if (inputY < 0)
+        {
+            flippedVAR = Flipped.flippedDown;
+        }
+        else if(inputY > 0){
+            flippedVAR= Flipped.flippedUp;
+        }
+
         if (FoodAmount >= 5)
         {
-            parentScript.CollectMask();
-            MaskPickup.MinigamingIt = false;
-            Destroy(this.gameObject);
+            //parentScript.CollectMask();
+            //MaskPickup.MinigamingIt = false;
+            Destroy(_snakeGame);
         }
     }
 }
