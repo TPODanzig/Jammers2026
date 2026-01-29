9d0f57c1e72617b42a39f0943e8469a40801ed6b
proximty <hanna@versluizen.nl>
2026-01-29 19:48:19 +0100

    masks

--- Assets/Scripts/Kaj/XrayScript.cs
+++ Assets/Scripts/Kaj/XrayScript.cs
@@ -1,11 +1,26 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class XrayScript : MonoBehaviour
 {
     [SerializeField] Transform DitEneXrayMuurtje;
     //public int XrayActiveInt = 0;
     public bool XrayActive = false;
+    public GameObject Volume;
+    public bool XRayMaskActive;
+
+    private void Update()
+    {
+        if (XRayMaskActive)
+        {
+            Run();
+        }
+        else
+        {
+            NoRun();
+        }
+    }
 
     void FixedUpdate()
     {
@@ -32,13 +47,26 @@ public class XrayScript : MonoBehaviour
 
     public void XrayActivate()
     {
+        
         if (XrayActive)
         {
+            
             DitEneXrayMuurtje.GetComponent<MeshRenderer>().enabled = false;
         }
         else if(!XrayActive)
         {
+            
             DitEneXrayMuurtje.GetComponent<MeshRenderer>().enabled = true;
         }
+    }
+    private void Run()
+    {
+        Volume.SetActive(true);
+       
+    }
+    private void NoRun()
+    {
+        Volume.SetActive(false);
+        
     }
 }
