bec88ff8234ce67c80a0213631a8324ed77cb15a
TPODanzig <tpodanzig@gmail.com>
2026-01-29 12:11:43 +0100

    AUDIO WERKT

---
+++ Assets/Scripts/Robin/AudioPlayer.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+
+    public enum AudioResource
+    {
+        NULL,
+        EquipMask
+    }
+    public AudioResource AudioPlayerResource;
+
+    [SerializeField] private AudioClip equipMask;
+
+    private AudioSource aPlayer;
+
+    private bool AudioPlayed;
+
+    private float DestructionTimer = 3;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        aPlayer = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if (AudioPlayerResource == AudioResource.NULL)
+        {
+            DestructionTimer = DestructionTimer - Time.deltaTime;
+
+            if (DestructionTimer < 0)
+            {
+                Debug.Log("NullAudio, cannot play");
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            //Select the clip
+            if (AudioPlayerResource == AudioResource.EquipMask)
+            {
+                aPlayer.clip = equipMask;
+            }
+        }
+
+        if (!AudioPlayed & !aPlayer.isPlaying)
+        {
+            aPlayer.Play();
+        }
+        else if (aPlayer.isPlaying)
+        {
+            AudioPlayed = true;
+        }
+        else if (AudioPlayed & !aPlayer.isPlaying)
+        {
+            //if audio has played, and has now stopped, destroy the player
+            Destroy(this.gameObject);
+        }
+    }
+}
