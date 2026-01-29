18356b83b231ad85f9974731560fe0ccad56c834
TPODanzig <tpodanzig@gmail.com>
2026-01-29 11:40:42 +0100

    MASKS WERKEN

--- Assets/Scripts/hanna/BlackLight effect.cs
+++ Assets/Scripts/hanna/BlackLight effect.cs
@@ -6,11 +6,40 @@ using UnityEngine.UI;
 
 public class BlackLighteffect : MonoBehaviour
 {
+    public bool BLMaskActive;
+    public GameObject Volume;
+    public InputActionReference blacklight;
+    public Image Fill;
     bool light;
-    public  GameObject Light;
-    private void Start()
-    {
-        if (Light.CompareTag("Light")) {  light = true; }
+    public GameObject Light;
 
+    private void Update()
+    {
+        if (BLMaskActive)
+        {
+            Run();
+        }
+        else
+        {
+            NoRun();
+        }
+    }
+
+    private void Run()
+    {
+        Volume.SetActive(true);
+        if (Light.CompareTag("Light")) 
+        { 
+            light = true;
+        }
+    }
+
+    private void NoRun()
+    {
+        Volume.SetActive(false);
+        if (Light.CompareTag("Light"))
+        {
+            light = false;
+        }
     }
 }
