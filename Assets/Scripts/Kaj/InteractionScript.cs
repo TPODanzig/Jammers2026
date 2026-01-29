5f20ff5c3f63c2dd0775a5abec610956bc827764
ktpca2 <kajlevidevries@gmail.com>
2026-01-29 16:37:34 +0100

    INTERACTION YAYYYYY

---
+++ Assets/Scripts/Kaj/InteractionScript.cs
@@ -0,0 +1,33 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionScript : MonoBehaviour
+{
+    public GameObject InteractionText;
+
+    private void Start()
+    {
+        InteractionText.SetActive(false);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Interactable"))
+        {
+            //Debug.Log("bitch");
+            InteractionText.SetActive(true);
+            if (Input.GetKey("e"))
+            {
+                Destroy(other.gameObject);
+                InteractionText.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("lasagna");
+        InteractionText.SetActive(false);
+    }
+}
