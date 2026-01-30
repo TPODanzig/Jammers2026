8eceacba9df2cf2bbcf1db60c9925b1e87bf94a5
proximty <hanna@versluizen.nl>
2026-01-30 06:32:44 +0100

    kameloen

--- Assets/Scripts/hanna/minigame/camo minigame/manager.cs
+++ Assets/Scripts/hanna/minigame/camo minigame/manager.cs
@@ -1,18 +1,49 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class manager : MonoBehaviour
+public class SpriteMatchGame : MonoBehaviour
 {
-    public static manager manger;
-    public  int selectedcolor;
-    public GameObject[] Colors;
-    public GameObject[] items;
-    private void Awake()
-    {
-        manger = this;
-        selectedcolor = Random.Range(0, Colors.Length);
-        //voor de Kameleon    1 = groen   -   2 = oranje   -   3 = rood
+    public Image targetImage; // de sprite die bovenaan staat
+    public Button[] buttons; // de buttons die je moet matchen
+    public Sprite[] sprites; // alle mogelijke sprites
+    private Sprite currentTarget;
 
-        //voor de items       1 = groen   -   2 = oranje   -   3 = rood   -   4 = blauw
+    void Start()
+    {
+        SetupButtons();
+        SetRandomTarget();
     }
 
+    void SetupButtons()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            int index = i; // belangrijke trick voor closures
+            buttons[i].onClick.AddListener(() => CheckMatch(index));
+            buttons[i].image.sprite = sprites[i]; // zet sprite op de button
+        }
+    }
+
+    void SetRandomTarget()
+    {
+        int randomIndex = Random.Range(0, sprites.Length);
+        currentTarget = sprites[randomIndex];
+        targetImage.sprite = currentTarget;
+    }
+
+    void CheckMatch(int buttonIndex)
+    {
+        if (buttons[buttonIndex].image.sprite == currentTarget)
+        {
+            Debug.Log("Correct!");
+            SetRandomTarget(); // nieuwe target
+        }
+        else
+        {
+            Debug.Log("Fout! Probeer opnieuw.");
+        }
+    }
 }
+
+
+
