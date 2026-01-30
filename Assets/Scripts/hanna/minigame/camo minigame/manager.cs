8ec048ac3e54d98c68eed896f8aceca7ad9154df
TPODanzig <tpodanzig@gmail.com>
2026-01-30 03:55:34 +0100

    Kameleon vermooit

--- Assets/Scripts/hanna/minigame/camo minigame/manager.cs
+++ Assets/Scripts/hanna/minigame/camo minigame/manager.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 public class manager : MonoBehaviour
 {
     public static manager manger;
-   public  int selectedcolor;
-   public GameObject[] Colors;
-   public GameObject[] items;
+    public  int selectedcolor;
+    public GameObject[] Colors;
+    public GameObject[] items;
     private void Awake()
     {
         manger = this;
         selectedcolor = Random.Range(0, Colors.Length);
+        //voor de Kameleon    1 = groen   -   2 = oranje   -   3 = rood
+
+        //voor de items       1 = groen   -   2 = oranje   -   3 = rood   -   4 = blauw
     }
 
 }
